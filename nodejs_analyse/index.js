08bc807b49491e545c9076ab76f43c2e8b038198
Prattaykun <prattaystudent@gmail.com>
2025-12-31 04:04:31 +0530

    fix: update CORS configuration to allow requests from the production URL

--- nodejs_analyse/index.js
+++ nodejs_analyse/index.js
@@ -12,7 +12,7 @@ const app = express();
 
 /*  ENABLE CORS */
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", "https://beyondchat-scrapping.vercel.app/"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: false
 }));
